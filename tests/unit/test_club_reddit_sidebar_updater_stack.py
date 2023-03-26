b8e22ba0cc851e561a52f0738d210b8db9e24970
Michael Foley <folem@amazon.com>
2023-03-26 09:13:54 -0400

    Initial Commit CDK Python

---
+++ tests/unit/test_club_reddit_sidebar_updater_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from club_reddit_sidebar_updater.club_reddit_sidebar_updater_stack import ClubRedditSidebarUpdaterStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in club_reddit_sidebar_updater/club_reddit_sidebar_updater_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = ClubRedditSidebarUpdaterStack(app, "club-reddit-sidebar-updater")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
